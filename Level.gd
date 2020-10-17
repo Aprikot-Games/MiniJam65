b3d83ee88a22a4137e5ff0d2ec4be12a04a6387b
Gecko05 <jacacent@gmail.com>
2020-10-16 19:24:47 -0500

    Proto1

--- Level.gd
+++ Level.gd
@@ -2,16 +2,48 @@ extends Node2D
 
 export (PackedScene) var Row
 
+signal player_tick
+
 var n_rows = 0
+var rows = []
 const ROW_HEIGHT = 80
-# Called when the node enters the scene tree for the first time.
+const SPRITE_WIDTH = 80
+
+var player_pos = [0, 0]
+
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func generate(layout):
 	n_rows = layout[0] 
 	for i in range(n_rows):
-		
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+		var new_row = Row.instance()
+		add_child(new_row)
+		new_row.generate()
+		new_row.speed = i
+		new_row.position.y = -(i * ROW_HEIGHT)
+		rows.append(new_row)
+		connect("player_tick", new_row, "on_player_tick")
+		$Player.raise()
+	$Timer.start()
+
+func _process(delta):
+	var emit = true
+	if Input.is_action_just_pressed("ui_up"):
+		player_pos[0] -= 1
+	elif Input.is_action_just_pressed("ui_down"):
+		player_pos[0] += 1
+	elif Input.is_action_just_pressed("ui_left"):
+		player_pos[1] -= 1
+	elif Input.is_action_just_pressed("ui_right"):
+		player_pos[1] += 1
+	else:
+		emit = false
+	if emit == true:
+		#emit_signal("player_tick")
+		$Player.position.x = (player_pos[1] * SPRITE_WIDTH)
+		$Player.position.y = (player_pos[0] * SPRITE_WIDTH) - 2
+		print(player_pos)
+
+func _on_Timer_timeout():
+	emit_signal("player_tick")
--- Row.gd
+++ Row.gd
@@ -19,6 +19,7 @@ var dino_tex = preload("res://sprites/dino.png")
 
 var n_blocks = 6
 var speed = 1
+var tick_cnt = 0
 const SPRITE_WIDTH = 80
 var row_width = n_blocks * SPRITE_WIDTH
 var rng = RandomNumberGenerator.new()
@@ -26,6 +27,9 @@ var rng = RandomNumberGenerator.new()
 var row = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pass
+
+func generate():
 	rng.randomize()
 	for i in range(n_blocks):
 		var new_block = Block.instance()
@@ -36,5 +40,17 @@ func _ready():
 		new_block.position.x = i * SPRITE_WIDTH - (row_width/2)
 		row.append(new_block)
 
-func _on_tick():
-	pass
+func rotate_left():
+	var end_block = row[0]
+	for i in range(1, n_blocks):
+		row[i-1] = row[i]
+		row[i-1].position.x -= SPRITE_WIDTH
+	row[n_blocks-1] = end_block
+	row[n_blocks-1].position.x += (n_blocks-1) * SPRITE_WIDTH
+
+func on_player_tick():
+	print("Player Move!")
+	tick_cnt += 1
+	if tick_cnt >= speed:
+		rotate_left()
+		tick_cnt = 0
